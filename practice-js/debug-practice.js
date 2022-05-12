87e3a8bcfa455bef2567a4d6c1606bc6d7c19831
G186872021 <218687@st.takushoku-u.ac.jp>
2022-05-12 13:18:54 +0900

    ok

--- practice-js/debug-practice.js
+++ practice-js/debug-practice.js
@@ -10,11 +10,12 @@ console.log(ary[2]);        // 3番目の要素は存在しない
 
 // (4) 小数のインデックス       → undefined　（エラーにならない）
 let i = 3/2;
+Math.floor(i);
 console.log(ary[i]);        // i=3/2=1.5 なので ary[1.5] を参照しようとする
 
 // (5) メンバー名の間違い       → undefined （エラーにならない）
 let obj = {x:3, y:7};
-console.log(obj.z);         // メンバー z は存在しない
+console.log(obj.x);         // メンバー z は存在しない
 
 // (6) 初期化していない変数のメンバー   → エラー発生
 let o;
