ca5f8a0c4a44bfb5ad77e07e6c8fc7e88df063b3
G186872021 <103618264+G186872021@users.noreply.github.com>
2022-05-17 10:14:29 +0900

    ok

--- kazuate.js
+++ kazuate.js
@@ -17,17 +17,24 @@ hantei();
 function hantei() {
     let yoso = 4;       // 第5回課題:テキストボックスの数値をここに代入
     
-    for (let a = 1; a===kaisu; a++) {
+    for (let a = 1; a===kaisu||kotae===yoso; a++) {
+        console.log(a,'回目の予想: ',yoso)
         if (kotae!==yoso) {
             if (kotae>=yoso) {
                 console.log('まちがい。答えはもっと大きいですよ。');
             } else if (kotae<=yoso) {
                 console.log('まちがい。答えはもっと小さいですよ。');
-            } else if (a=kaisu) {
-                console.log('まちがい。残念でした答えは',kotae,'です。')
+            } else if (a===kaisu) {
+                console.log('まちがい。残念でした答えは ',kotae,' です。');
             }
-        } else {
-            console.log('正解です。おめせとう！');
+        }
+
+        if (kotae===yoso) {
+            console.log('正解です。おめでとう!');
+        }
+
+        if (kotae===yoso) {
+            console.log('答えは ',kotae,' でした。すでにゲームは終わっています。');
         }
     }
     //        ページに表示する方法はまだ習っていないので
