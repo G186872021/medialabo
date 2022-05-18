d04e9d25f06ecd6f9f9d8db3b47d909da6893595
G186872021 <103618264+G186872021@users.noreply.github.com>
2022-05-19 00:42:37 +0900

    ok

--- kazuate.js
+++ kazuate.js
@@ -16,18 +16,21 @@ hantei();
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
     let yoso = 4;       // 第5回課題:テキストボックスの数値をここに代入
-    if (kaisu<2) {
-        if (yoso===kotae) {
-            console.log('正解です。おめでとう!');
-        } else if (kotae>yoso) {
-            console.log('まちがい。答えはもっと大きいですよ。');
-        } else if (kotae<yoso) {
-            console.log('まちがい。答えはもっと小さいですよ。');
-        }
-    } else if (kaisu===2) {
-        console.log('まちがい。残念でした答えは '+kotae+' です。');
+
+    if (yoso===kotae) {
+        console.log('正解です。おめでとう!');
     } else {
-        console.log('答えは '+kotae+' でした。すでにゲームは終わっています。');
+        if (kaisu<2) {
+            if (kotae>yoso) {
+                console.log('まちがい。答えはもっと大きいですよ。');
+            } else if (kotae<yoso) {
+                console.log('まちがい。答えはもっと小さいですよ。');
+            }
+        } else if (kaisu===2) {
+            console.log('まちがい。残念でした答えは '+kotae+' です。');
+        } else {
+            console.log('答えは '+kotae+' でした。すでにゲームは終わっています。');
+        }
     }
     kaisu = kaisu + 1;
 
