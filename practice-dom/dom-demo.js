d8fa63a9d3f95902f5e41aec18c93d70475ed454
G186872021 <218687@st.takushoku-u.ac.jp>
2022-05-19 14:19:49 +0900

    ok

--- practice-dom/dom-demo.js
+++ practice-dom/dom-demo.js
@@ -45,5 +45,5 @@ for (let o of oceans) {
 let h2s = document.querySelector('h2');
 
 for (let h2 of h2s) {
-    h2.style.backgroundColor = 'palegreen';
+    h2s.style.backgroundColor = 'palegreen';
 }
