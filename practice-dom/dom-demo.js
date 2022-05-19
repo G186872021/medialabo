4e18e5f3d97a581e045a0d7219339ea527a31a50
G186872021 <218687@st.takushoku-u.ac.jp>
2022-05-19 12:47:24 +0900

    ok

--- practice-dom/dom-demo.js
+++ practice-dom/dom-demo.js
@@ -14,7 +14,7 @@ ul.insertAdjacentElement('beforeend', li);
 
 // id が sevenOcean の h2 要素を検索
 // （次の行を完成させる）
-let h2sevenOcean = document.querySelector('h2');
+let h2sevenOcean = document.querySelector('h2#savenOcean');
 
 // 7大洋　の名前を配列に代入
 let oceans = [
