8c0b65bbc5fa6e0ac38a3411c0c3b9e444c01153
G186872021 <218687@st.takushoku-u.ac.jp>
2022-05-19 12:36:31 +0900

    ok

--- practice-dom/dom-demo.js
+++ practice-dom/dom-demo.js
@@ -1,9 +1,13 @@
-let ul = document.querySelector('ul');
+let eu = document.querySelector('li#EU');
+eu.remove()
 
+let ul = document.querySelector('ul');
 let li;
 
+li = document.createElement('li');
 li.textContent = 'アジア大陸'; 
 ul.insertAdjacentElement('beforeend', li);
 
-let eu = document.querySelector('li#EU');
-eu.remove()
+li = document.createElement('li');
+li.textContent = 'ヨーロッパ大陸';
+ul.insertAdjacentElement('beforeend', li);
