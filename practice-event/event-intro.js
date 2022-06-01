e034ed51e452daa0e6c9530a6c4298efb4443820
G186872021 <103618264+G186872021@users.noreply.github.com>
2022-06-01 21:57:34 +0900

    ok

--- practice-event/event-intro.js
+++ practice-event/event-intro.js
@@ -5,8 +5,9 @@ let b1 = document.querySelector('button#blue');
 b1.addEventListener('click', changeH1ColorRed);
 
 //// 以下，練習5-2 でコードを追加
-
-
+let b2 = document.querySelector('button#random');
+b2.addEventListener('click', changeBackgroundColorRandom);
+b1.addEventListener('click', changeH1ColorRed);
 ////////////////////////////////////////////////////////
 // 2. イベントハンドラ（イベントが発生したら実行する関数）
 
