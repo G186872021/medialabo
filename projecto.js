3f7bcb756605440fd2a1ffdd06e9bb7c89daa442
G186872021 <103618264+G186872021@users.noreply.github.com>
2022-06-06 12:23:23 +0900

    ok

--- projecto.js
+++ projecto.js
@@ -3,8 +3,9 @@ let a = document.querySelector('#kensaku');
 a.addEventListener('click', hantei);
 
 function hantei() {
+    let name = prompt();
     let p = document.querySelector('p#test');
-    p.textContent = kensaku;
+    p.textContent = name;
 }
 //都市を探す
 //idに当てはめる
