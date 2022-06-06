a11cdf6089c588aff768f106ba4347d1c0e6b7e1
G186872021 <103618264+G186872021@users.noreply.github.com>
2022-06-06 12:36:08 +0900

    ok

--- projecto.js
+++ projecto.js
@@ -1,13 +1,17 @@
 //クリックした後の操作
 let a = document.querySelector('#kensaku');
-a.addEventListener('click', hantei);
+a.addEventListener('click', kensaku);
 
-
-let name = prompt();
-let p = document.querySelector('p#test');
-
-function hantei() {
-    p.textContent = name;
+function kensaku() {
+    let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/{id}.json';
 }
+function kensaku(resp) {
+    let data = resp.deta;
+    
+}
+
 //都市を探す
+
 //idに当てはめる
+
+}
