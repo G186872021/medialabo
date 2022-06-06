cfe87c6afe6e20aac6e8aaebe20b9dc169c490e0
G186872021 <103618264+G186872021@users.noreply.github.com>
2022-06-06 12:12:06 +0900

    ok

--- projecto.js
+++ projecto.js
@@ -2,5 +2,9 @@
 let a = document.querySelector('#kensaku');
 a.addEventListener('click', hantei);
 
+function hantei() {
+    let tosi = prompt("都市の名前を入力");
+    para.textContent = tosi;
+}
 //都市を探す
 //idに当てはめる
