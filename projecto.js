6ed904d38de828b75d640bcab0410373232a232d
G186872021 <103618264+G186872021@users.noreply.github.com>
2022-06-08 21:39:44 +0900

    ok

--- projecto.js
+++ projecto.js
@@ -57,7 +57,7 @@ function showResult(resp) {
     console.log(data.x);
 
     let box_1 = document.querySelector('div#name');
-    box_1.textContent = 'aaaa';
+    box_1.textContent = data.name;
 
     let box_2 = document.querySelector('div#weather.description');
     box_2.textContent = weather.description;
